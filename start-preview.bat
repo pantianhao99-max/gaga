3deb04350c0941b16ea9592efa89447bcb6d30f7
pantianhao99-max <pantianhao99@gmail.com>
2026-04-10 16:41:19 +0800

    Add local preview launcher

---
+++ start-preview.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+set PORT=8109
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -WindowStyle Hidden -ArgumentList '-NoProfile','-ExecutionPolicy','Bypass','-File','%~dp0serve-static.ps1','-Port','%PORT%'"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Sleep -Seconds 2"
+start "" "http://localhost:%PORT%/"
+echo Preview launching at http://localhost:%PORT%/
+exit /b 0
